48e3fb45727329273c5568197e73b6a8a476f9e6
pranavjayan <119648199+pranavjayan@users.noreply.github.com>
2025-02-23 19:40:25 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://gym-management-app-backend.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
