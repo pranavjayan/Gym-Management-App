5b73c7e127087ebdc926ce56dea2af8eca48fae4
pranavjayan <119648199+pranavjayan@users.noreply.github.com>
2025-02-23 19:31:34 +0530

    Update ClassScheduling.jsx

--- frontend/src/components/ClassScheduling.jsx
+++ frontend/src/components/ClassScheduling.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/classes";
+const API_URL = "https://gym-management-app-backend.onrender.com/api/classes";
 
 const ClassScheduling = () => {
   const [classes, setClasses] = useState([]);
